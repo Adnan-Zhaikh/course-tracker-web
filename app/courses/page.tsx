1744742ec1255ed00b9336e88ae9c7429db30c58
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-05 21:27:29 +0530

    courses page with real data from database

---
+++ app/courses/page.tsx
@@ -0,0 +1,27 @@
+import Sidebar from "@/components/sidebar";
+import { prisma } from "@/lib/prisma";
+
+export default async function CoursePage() {
+    const courses = await prisma.course.findMany();
+
+return (
+  <div className="flex h-screen bg-gray-100">
+    <Sidebar />
+    <main className="flex-1 p-8">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">Courses</h1>
+      
+        <div className="grid grid-cols-1 gap-6">
+        {courses.map((course) => (
+            <div key={course.id} className="bg-white rounded-xl shadow p-6">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">{course.title}</h2>
+            <p className="text-sm text-gray-500 mt-2 h-line">{course.description}</p>
+            <p className="text-sm text-gray-500 mt-2">{course.totalLectures} Lectures</p>
+            </div>
+            ))}
+        </div>
+      
+    </main>
+  </div>
+);
+}
+
